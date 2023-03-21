d2eb69e6cf72c51c2a144922a50bda124c1a0243
DiamondD7 <aaron.diamond2@yahoo.co.nz>
2023-03-21 23:13:05 +1300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import SignIn from "./components/SignIn/SignIn";
+
+const App = () => {
+  return (
+    <div>
+      <div>
+        <SignIn />
+      </div>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/SignIn/SignIn.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+import "../../App.css";
+const SignIn = () => {
+  return (
+    <div>
+      <h1>This is the signin component</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum
+        repudiandae dolorem temporibus quibusdam ratione error omnis similique
+        eum dicta delectus. Debitis voluptatem aperiam temporibus alias labore
+        perferendis dicta deserunt similique.
+      </p>
+    </div>
+  );
+};
+
+export default SignIn;
