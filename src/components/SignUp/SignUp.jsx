7f4f1c6820501e7dc9a86453a57ad7dbbb0783e5
DiamondD7 <aaron.diamond2@yahoo.co.nz>
2023-03-24 09:21:29 +1300

    still implementing the sign up component

--- src/components/SignUp/SignUp.jsx
+++ src/components/SignUp/SignUp.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 
+import "../../styles/signupstyles.css";
 const SignUp = () => {
   return (
     <div>
-      <h1>Hello, World!</h1>
+      <div className="signup-main__wrapper">
+        <h1>Register</h1>
+      </div>
     </div>
   );
 };
