daedd212f5a4c48c2ce2f13aa6a471237846fb00
DiamondD7 <aaron.diamond2@yahoo.co.nz>
2023-03-24 09:35:01 +1300

    saving the current progress; forms

--- src/components/SignUp/SignUp.jsx
+++ src/components/SignUp/SignUp.jsx
@@ -5,7 +5,28 @@ const SignUp = () => {
   return (
     <div>
       <div className="signup-main__wrapper">
-        <h1>Register</h1>
+        <div>
+          <h1>Register</h1>
+          <div className="form-main-container__wrapper">
+            <form>
+              <div>
+                <input type="text" placeholder="First name" />
+                <br />
+                <input type="text" placeholder="Last name" />
+              </div>
+
+              <div>
+                <input type="text" placeholder="Email" />
+                <br />
+                <input type="text" placeholder="Mobile number" />
+              </div>
+
+              <div>
+                <input type="text" placeholder="Username" />
+              </div>
+            </form>
+          </div>
+        </div>
       </div>
     </div>
   );
