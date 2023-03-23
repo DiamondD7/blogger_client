bf9b8f445efaa40f428e4595be25cb4fd90c746c
DiamondD7 <aaron.diamond2@yahoo.co.nz>
2023-03-24 09:06:09 +1300

    implemented a way to go to the sign up page

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import { Routes, Route } from "react-router-dom";
 import SignIn from "./components/SignIn/SignIn";
 
 import "./App.css";
+import HomePage from "./components/HomePage/HomePage";
+import SignUp from "./components/SignUp/SignUp";
 const App = () => {
+  const [authorized, setAuthorized] = useState(false);
   return (
     <div>
       <div>
-        <SignIn />
+        {authorized ? (
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/signup" element={<SignUp />} />
+          </Routes>
+        ) : (
+          <SignIn />
+        )}
       </div>
     </div>
   );
--- src/components/SignIn/SignIn.jsx
+++ src/components/SignIn/SignIn.jsx
@@ -1,22 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
+import SigninForm from "./Signin-form";
+import { Routes, Route, Link } from "react-router-dom";
 
 import "../../styles/signinstyles.css";
-import SigninForm from "./Signin-form";
+import SignUp from "../SignUp/SignUp";
 const SignIn = () => {
+  const [goToSignIn, setGoToSignIn] = useState(false);
   return (
     <div>
-      <div className="signin-main__wrapper">
-        <div>
-          <h1>Sign In</h1>
-          <SigninForm />
-
+      {goToSignIn ? (
+        <SignUp />
+      ) : (
+        <div className="signin-main__wrapper">
           <div>
-            <p className="p-alreadyhaveaccount__text">
-              Already have an account? Click <a href="#">here</a>
-            </p>
+            <h1>Sign In</h1>
+            <SigninForm />
+
+            <div>
+              <p className="p-alreadyhaveaccount__text">
+                Already have an account? Click{" "}
+                <a
+                  className="gosignin-anchor"
+                  onClick={() => setGoToSignIn(true)}
+                >
+                  here
+                </a>
+              </p>
+            </div>
           </div>
         </div>
-      </div>
+      )}
     </div>
   );
 };
