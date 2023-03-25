f3f66e2cd25e1349a9e5a83a1391466cfa0b9b2c
DiamondD7 <aaron.diamond2@yahoo.co.nz>
2023-03-26 00:04:05 +1300

    partially finished the styling of the sign up form

--- src/components/SignUp/SignUp.jsx
+++ src/components/SignUp/SignUp.jsx
@@ -9,20 +9,50 @@ const SignUp = () => {
           <h1>Register</h1>
           <div className="form-main-container__wrapper">
             <form>
-              <div>
-                <input type="text" placeholder="First name" />
+              <div className="user-fname-lname__wrapper">
+                <input
+                  className="user-fname__input"
+                  type="text"
+                  placeholder="First name"
+                />
                 <br />
-                <input type="text" placeholder="Last name" />
+                <input
+                  className="user-lname__input"
+                  type="text"
+                  placeholder="Last name"
+                />
               </div>
 
-              <div>
-                <input type="text" placeholder="Email" />
-                <br />
-                <input type="text" placeholder="Mobile number" />
+              <div className="user-email__wrapper">
+                <input
+                  className="user-email__input"
+                  type="text"
+                  placeholder="Email"
+                />
               </div>
 
-              <div>
-                <input type="text" placeholder="Username" />
+              <div className="user-mobile-gender__wrapper">
+                <input
+                  className="user-mobile__input"
+                  type="text"
+                  placeholder="Mobile number"
+                />
+
+                <select className="user-gender__select">
+                  <option>Gender</option>
+                  <option></option>
+                  <option>Female</option>
+                  <option>Male</option>
+                  <option>Other</option>
+                </select>
+              </div>
+
+              <div className="user-username__wrapper">
+                <input
+                  className="user-username__input"
+                  type="text"
+                  placeholder="Username"
+                />
               </div>
 
               <div>
