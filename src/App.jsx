67597391ad0788a10670eb2ce1192e7d32e6eed6
DiamondD7 <aaron.diamond2@yahoo.co.nz>
2023-03-26 21:27:24 +1300

    added an animation when clicking to next phase of the sign up form

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,9 @@ import React, { useState } from "react";
 import { Routes, Route } from "react-router-dom";
 import SignIn from "./components/SignIn/SignIn";
 
-import "./App.css";
 import HomePage from "./components/HomePage/HomePage";
 import SignUp from "./components/SignUp/SignUp";
+import "./App.css";
 const App = () => {
   const [authorized, setAuthorized] = useState(false);
   return (
@@ -16,7 +16,9 @@ const App = () => {
             <Route path="/signup" element={<SignUp />} />
           </Routes>
         ) : (
-          <SignIn />
+          <div className="signin-app__container">
+            <SignIn />
+          </div>
         )}
       </div>
     </div>
--- src/components/SignUp/SignUp.jsx
+++ src/components/SignUp/SignUp.jsx
@@ -1,7 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "../../styles/signupstyles.css";
 const SignUp = () => {
+  const [nextPhase, setNextPhase] = useState(false);
+
+  const transition = nextPhase
+    ? {
+        right: "50%",
+        top: "30%",
+        transform: "translate(50%, -50%)",
+        display: "block",
+      }
+    : {};
   return (
     <div>
       <div className="signup-main__wrapper">
@@ -9,60 +19,68 @@ const SignUp = () => {
           <h1>Register</h1>
           <div className="form-main-container__wrapper">
             <form>
-              <div className="user-fname-lname__wrapper">
-                <input
-                  className="user-fname__input"
-                  type="text"
-                  placeholder="First name"
-                />
-                <br />
-                <input
-                  className="user-lname__input"
-                  type="text"
-                  placeholder="Last name"
-                />
+              <div
+                className={`default ${nextPhase === true ? "slideLeft" : ""}`}
+              >
+                <div className="user-fname-lname__wrapper">
+                  <input
+                    className="user-fname__input"
+                    type="text"
+                    placeholder="First name"
+                  />
+                  <br />
+                  <input
+                    className="user-lname__input"
+                    type="text"
+                    placeholder="Last name"
+                  />
+                </div>
+
+                <div className="user-email__wrapper">
+                  <input
+                    className="user-email__input"
+                    type="text"
+                    placeholder="Email"
+                  />
+                </div>
+
+                <div className="user-mobile-gender__wrapper">
+                  <input
+                    className="user-mobile__input"
+                    type="text"
+                    placeholder="Mobile number"
+                  />
+
+                  <select className="user-gender__select">
+                    <option>Gender</option>
+                    <option></option>
+                    <option>Female</option>
+                    <option>Male</option>
+                    <option>Other</option>
+                  </select>
+                </div>
+
+                <div className="user-username__wrapper">
+                  <input
+                    className="user-username__input"
+                    type="text"
+                    placeholder="Username"
+                  />
+                </div>
+
+                <div className="signup-btn__wrapper">
+                  <button
+                    type="button"
+                    className="signup-btn__submit"
+                    onClick={() => setNextPhase(!nextPhase)}
+                  >
+                    Next
+                  </button>
+                </div>
               </div>
 
-              <div className="user-email__wrapper">
-                <input
-                  className="user-email__input"
-                  type="text"
-                  placeholder="Email"
-                />
-              </div>
-
-              <div className="user-mobile-gender__wrapper">
-                <input
-                  className="user-mobile__input"
-                  type="text"
-                  placeholder="Mobile number"
-                />
-
-                <select className="user-gender__select">
-                  <option>Gender</option>
-                  <option></option>
-                  <option>Female</option>
-                  <option>Male</option>
-                  <option>Other</option>
-                </select>
-              </div>
-
-              <div className="user-username__wrapper">
-                <input
-                  className="user-username__input"
-                  type="text"
-                  placeholder="Username"
-                />
-              </div>
-
-              <div>
+              <div className="nextphase__wrapper" style={transition}>
                 <NextRegisterPhase />
-              </div>
-
-              <div className="signup-btn__wrapper">
-                <button type="button" className="signup-btn__submit">
-                  Next
-                </button>
               </div>
             </form>
           </div>
