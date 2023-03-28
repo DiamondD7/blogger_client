2145e8d710a7cc183cf85fa313d21812fb39fa11
DiamondD7 <aaron.diamond2@yahoo.co.nz>
2023-03-28 22:59:07 +1300

    little tweaks in the home contents

--- src/components/HomePage/HomeContent.jsx
+++ src/components/HomePage/HomeContent.jsx
@@ -31,7 +31,7 @@ const HomeContent = () => {
       <div className="content-main__wrapper">
         <img
           className="image-blog"
-          src="https://images.unsplash.com/photo-1547347298-4074fc3086f0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
+          src="https://images.unsplash.com/photo-1538115081112-32c7d8401807?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
           alt="example pic"
         />
 
@@ -43,10 +43,10 @@ const HomeContent = () => {
             transition: "all 0.2s ease-in",
           }}
         >
-          <h1>Volleyball club champs to hit Tauranga</h1>
+          <h1>The Police are rioting. We need to talk about it.</h1>
           <div className="content-details__wrapper">
-            <p>The Bay's News First</p>
-            <p>8/12/2023</p>
+            <p>The New York Times</p>
+            <p>5/12/2023</p>
           </div>
         </Link>
       </div>
