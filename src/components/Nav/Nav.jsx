f2a0094905ee0f51bff4839353a2fb4e1ea8b9f8
DiamondD7 <aaron.diamond2@yahoo.co.nz>
2023-03-28 22:16:15 +1300

    navigation styles implemented

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,9 +1,25 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
+import "../../styles/navstyles.css";
 
 const Nav = () => {
   return (
     <div>
-      <h1>Hello</h1>
+      <ul className="nav-ul__wrapper">
+        <li>
+          <Link style={{ textDecoration: "none" }}>Home</Link>
+        </li>
+        <li>
+          <Link style={{ textDecoration: "none" }}>Profile</Link>
+        </li>
+        <li>
+          <Link style={{ textDecoration: "none" }}>Find Friends</Link>
+        </li>
+        <li>
+          <Link style={{ textDecoration: "none" }}>About</Link>
+        </li>
+      </ul>
     </div>
   );
 };
