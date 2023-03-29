18e11cce068e96e9dacab6b92ad505c21db1258c
DiamondD7 <aaron.diamond2@yahoo.co.nz>
2023-03-29 22:24:29 +1300

    added the profile to the routing

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import SignIn from "./components/SignIn/SignIn";
 import HomePage from "./components/HomePage/HomePage";
 import SignUp from "./components/SignUp/SignUp";
 import "./App.css";
+import Profile from "./components/Profile/Profile";
 const App = () => {
   const [authorized, setAuthorized] = useState(false);
   return (
@@ -23,6 +24,7 @@ const App = () => {
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/signup" element={<SignUp />} />
+          <Route path="/profile" element={<Profile />} />
         </Routes>
       </div>
     </div>
