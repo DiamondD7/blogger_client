d2bb7cbfa041dd331bd6d895bb50ba299939235e
DiamondD7 <aaron.diamond2@yahoo.co.nz>
2023-03-31 12:29:53 +1300

    implementing some features in the profile section

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import Nav from "../Nav/Nav";
 
+import "../../styles/profilestyles.css";
+
 const Profile = () => {
   return (
     <div>
@@ -8,7 +10,19 @@ const Profile = () => {
         <div>
           <Nav />
         </div>
-        <h1>Hello</h1>
+        <div className="profile-user-details__wrapper">
+          <img
+            className="profile-picture__image"
+            src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
+            alt="profile-picture"
+          />
+          <p>Jackie Simons</p>
+        </div>
+        <div className="profile-user-summary__wrapper">
+          <a href="#" style={{ textDecoration: "none" }}>
+            https://www.mysite.com
+          </a>
+        </div>
       </div>
     </div>
   );
