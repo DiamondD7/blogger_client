29c3f5684eac772d4036feb0ee56c8c7ad862098
DiamondD7 <aaron.diamond2@yahoo.co.nz>
2023-03-31 13:20:43 +1300

    finished styling and adding the followers count

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Nav from "../Nav/Nav";
+import { FiGlobe } from "react-icons/fi";
 
 import "../../styles/profilestyles.css";
 
@@ -11,17 +12,31 @@ const Profile = () => {
           <Nav />
         </div>
         <div className="profile-user-details__wrapper">
-          <img
-            className="profile-picture__image"
-            src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
-            alt="profile-picture"
-          />
-          <p>Jackie Simons</p>
+          <div>
+            <img
+              className="profile-picture__image"
+              src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
+              alt="profile-picture"
+            />
+          </div>
+          <div>
+            <div>
+              <p>Jackie Simons</p>
+            </div>
+
+            <div className="following-follower__wrapper">
+              <p className="following-text">100 following</p>
+              <p className="followers-text">277 followers</p>
+            </div>
+          </div>
         </div>
         <div className="profile-user-summary__wrapper">
-          <a href="#" style={{ textDecoration: "none" }}>
-            https://www.mysite.com
-          </a>
+          <div className="profile-user-summary-second__wrapper">
+            <FiGlobe size="17px" />
+            <a href="#" style={{ textDecoration: "none" }}>
+              https://www.mysite.com
+            </a>
+          </div>
         </div>
       </div>
     </div>
