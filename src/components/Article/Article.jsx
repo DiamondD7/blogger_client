4fadcbc3fd91b9bc7f5720e194febf227bc612a8
diamond <aaron.diamond2@yahoo.co.nz>
2023-05-29 18:49:26 +1200

    deleted display flex

--- src/components/Article/Article.jsx
+++ src/components/Article/Article.jsx
@@ -45,44 +45,6 @@ const Article = () => {
             amet, harum placeat, modi perspiciatis facere rerum consequuntur
             quas eius aperiam nulla ratione maxime itaque.
           </p>
-
-          <p className="paragrpah-1">
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laudantium
-            nesciunt ipsam voluptatum quis corrupti rerum, repudiandae sequi
-            consequuntur exercitationem fugiat eligendi, impedit ducimus
-            architecto nulla iste iusto inventore, modi voluptate. Lorem ipsum
-            dolor, sit amet consectetur adipisicing elit. Soluta quam facere
-            quas quos debitis, necessitatibus tenetur corrupti recusandae quae,
-            doloribus omnis, id tempora modi corporis itaque? Labore laudantium
-            tempora fuga. Lorem, ipsum dolor sit amet consectetur adipisicing
-            <br />
-            <br />
-            elit. Magnam, voluptas omnis! Magnam repellendus recusandae autem,
-            debitis, dolorem esse commodi ipsum molestias sequi nihil dolore a
-            aliquid quae consequuntur tempore quidem! Lorem ipsum dolor sit amet
-            consectetur adipisicing elit. Est, minus. Deserunt cumque quasi
-            error earum eum, sunt deleniti, est maxime quibusdam porro odio
-            aspernatur qui saepe rem omnis, doloremque assumenda? Lorem ipsum
-            dolor sit amet consectetur, adipisicing elit. Similique ipsam vero
-            at accusamus inventore, sunt error omnis eos sint minus consequatur
-            fugiat, natus possimus, eaque quaerat laboriosam autem qui. Tempore.
-            <br />
-            <br />
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Optio
-            iste, est asperiores quas vitae possimus perspiciatis quos deleniti
-            doloremque quae quam, maiores officia eveniet quibusdam ducimus quo?
-            Eaque, dolorem quidem. Lorem ipsum dolor sit, amet consectetur
-            adipisicing elit. Autem molestiae laborum, quibusdam eius cumque
-            dignissimos inventore quisquam velit, ipsa quo natus culpa,
-            excepturi omnis recusandae. Enim non perferendis alias ducimus!
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam aliquid
-            corrupti ducimus, iusto repellendus quod atque temporibus dolorem
-            quae excepturi perferendis adipisci ea assumenda tempora magnam nisi
-            aliquam iste quia. Lorem ipsum dolor sit amet, consectetur
-            adipisicing elit. Cum iusto quasi perferendis at possimus saepe
-            amet, harum placeat, modi perspiciatis facere rerum consequuntur
-            quas eius aperiam nulla ratione maxime itaque.
-          </p>
         </div>
       </div>
     </div>
