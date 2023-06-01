6091601cf1e6ca3c1ef7117edfbb0fd54259cd95
diamond <aaron.diamond2@yahoo.co.nz>
2023-06-01 22:43:47 +1200

    added a separate js file for all the API_URLs

---
+++ src/assets/js/API_AUTH.js
@@ -0,0 +1,7 @@
+const BASE_URL = 'https://localhost:7167/api';
+
+export const GET_USERS = `${BASE_URL}/GetAllUserData`;
+export const GET_A_USER = `${BASE_URL}/GetaUserData`;
+export const POST_USER = `${BASE_URL}/AddUserData`;
+export const PUT_USER = `${BASE_URL}/UpdateUserData`;
+export const DELETE_USER = `${BASE_URL}/DeleteUserData`;
