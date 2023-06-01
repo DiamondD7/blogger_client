b51fddb620f945db1e8354aabf97fb31ba78a14c
diamond <aaron.diamond2@yahoo.co.nz>
2023-06-01 23:17:21 +1200

    fixed my urls

--- src/assets/js/API_AUTH.js
+++ src/assets/js/API_AUTH.js
@@ -1,7 +1,8 @@
 const BASE_URL = 'https://localhost:7167/api';
+const USER_BASE_URL = `${BASE_URL}/User`
 
-export const GET_USERS = `${BASE_URL}/GetAllUserData`;
-export const GET_A_USER = `${BASE_URL}/GetaUserData`;
-export const POST_USER = `${BASE_URL}/AddUserData`;
-export const PUT_USER = `${BASE_URL}/UpdateUserData`;
-export const DELETE_USER = `${BASE_URL}/DeleteUserData`;
+export const GET_USERS = `${USER_BASE_URL}/GetAllUserData`;
+export const GET_A_USER = `${USER_BASE_URL}/GetaUserData`;
+export const POST_USER = `${USER_BASE_URL}/AddUserData`;
+export const PUT_USER = `${USER_BASE_URL}/UpdateUserData`;
+export const DELETE_USER = `${USER_BASE_URL}/DeleteUserData`;
