55fd08f58be4f6a12da83e9540d9d4c8d4de93bb
diamond <aaron.diamond2@yahoo.co.nz>
2023-06-10 22:50:41 +1200

    added the api urls for posts

--- src/assets/js/API_AUTH.js
+++ src/assets/js/API_AUTH.js
@@ -1,4 +1,6 @@
 const BASE_URL = 'https://localhost:7167/api';
+
+// USERS
 const USER_BASE_URL = `${BASE_URL}/User`
 
 export const GET_USERS = `${USER_BASE_URL}/GetAllUserData`;
@@ -7,3 +9,18 @@ export const VALIDATE_PASSWORD = `${USER_BASE_URL}/CheckPassword`;
 export const POST_USER = `${USER_BASE_URL}/AddUserData`;
 export const PUT_USER = `${USER_BASE_URL}/UpdateUserData`;
 export const DELETE_USER = `${USER_BASE_URL}/DeleteUserData`;
+
+// USERS
+
+
+
+
+// POSTS
+const POST_BASE_URL = `${BASE_URL}/Post`;
+
+export const GET_POSTS = `${POST_BASE_URL}/GetAllPosts`;
+export const ADD_POSTS = `${POST_BASE_URL}/AddPosts`;
+export const ADD_POST_IMAGE = `${POST_BASE_URL}/AddImage`;
+export const DELETE_POST = `${POST_BASE_URL}/DeletePosts`;
+
+// POSTS
